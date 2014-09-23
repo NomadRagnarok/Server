801afc2d0a85e251f96c4ce13f4b18270eb87b11
Snaehild <Snaehild@users.noreply.github.com>
2014-09-23 10:55:45 +0800

    [HERCULES] Fixes damage missing. Snap dodging bug. - Fixes damage miss on out of range Devotion. Follow-up: 964b473 - Fixes snap dodge bug. Added setting snap_dodge to return to old behavior. Bug report: http://hercules.ws/board/tracker/issue-3510-snap-dodge-bug/ https://github.com/HerculesWS/Hercules/commit/76662cc551f4256344640c91b2601923e15b2f7c

--- src/map/battle.h
+++ src/map/battle.h
@@ -7,7 +7,6 @@
 
 #include "map.h" //ELE_MAX
 #include "../common/cbasetypes.h"
-#include "mapreg.h"
 
 /**
  * Declarations
@@ -170,7 +169,6 @@ struct Battle_Config {
 	int emergency_call;
 	int guild_aura;
 	int pc_invincible_time;
-	int song_timer_reset;
 	
 	int pet_catch_rate;
 	int pet_rename;
@@ -476,6 +474,9 @@ struct Battle_Config {
 	int case_sensitive_aegisnames;
 	int guild_castle_invite;
 	int guild_castle_expulsion;
+
+	int song_timer_reset; // [csnv]
+	int snap_dodge; // Enable or disable dodging damage snapping away [csnv]
 };
 
 extern struct Battle_Config battle_config;
